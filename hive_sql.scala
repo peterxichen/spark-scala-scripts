d426eda21a4a17d1ae09d505efaad7c4de32ec56
peterxichen <peterxichen@aol.com>
2021-01-17 16:50:17 -0800

    hive sql setup

---
+++ hive_sql.scala
@@ -0,0 +1,17 @@
+// hive load
+import org.apache.spark.sql.hive.HiveContext
+
+val hiveCtx = new HiveContext(sc)
+val rows = hiveCtx.sql("
+    SELECT key, value
+    FROM sample_table
+")
+val keys = rows.map(row => row.getInt(0))
+
+// crate schemaRDD from case class
+
+case class HappyPerson(handel: String, favouriteBeverage: String)
+// create a person and turn it into a SchemaRDD
+val happyPeopleRDD = sc.parallelize(List(HappyPerson("holden", "coffee")))
+// implicit conversion - sqlCtx.createSchemaRDD(happyPeopleRDD)
+happyPeopleRDD.registertempTable("happy_table")
